c0206507de4ec2acce878eb9524a03fcfea62168
Terrell Cooper <terrellcoop@gmail.com>
2020-07-24 19:14:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,5 +6,8 @@ def key_for_min_value(hash)
   lowest_value = nil 
   hash.each do |key, value| 
   if lowest_value == nil || value < lowest_value
+    lowest_value = value
+    lowest_key = key 
+    
 
 end
